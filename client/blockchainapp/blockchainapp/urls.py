302bf6849674c68e3a79ae72daa9b2619d6e173c
0xmahadev@gmail.com <0xmahadev@gmail.com>
2021-02-20 23:47:41 +0545

    [edit] refactored frontend

--- client/blockchainapp/blockchainapp/urls.py
+++ client/blockchainapp/blockchainapp/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from voting.views import DashboardView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path("vote/", include("voting.urls")),
+    path("", DashboardView.as_view())
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
